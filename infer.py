27a3e4410a382d85ebfc39135a8666e214f64659
Tanishq Patil <trpatil@Tanishqs-MacBook-Pro.local>
2026-03-24 16:18:21 -0700

    final attempt

--- infer.py
+++ infer.py
@@ -413,6 +413,7 @@ def main():
             filtered_splats = predictions["splats"]
 
             # Flatten all views into one big splat array
+            print(len(filtered_splats))
             total_splats = filtered_splats["means"].shape[1]
             print(f"  - Saving splats for all views: {total_splats} total splats")
 
